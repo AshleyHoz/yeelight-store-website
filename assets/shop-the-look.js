2f8708e04f0f3038c0cd8cd71fa953acbf4ddfc0
何家伟 <hejiawei@yeelight.com>
2024-07-19 14:31:21 +0800

    添加lottie

--- assets/shop-the-look.js
+++ assets/shop-the-look.js
@@ -2,13 +2,29 @@
  *  @class
  *  @function ShopTheLook
  */
+import lottie from 'lottie-web';
+import loadingJson from '../assets/loading.json';
+
 if (!customElements.get('shop-the-look')) {
   class ShopTheLook extends HTMLElement {
-    constructor() {
+    constructor(e) {
       super();
+      console.log(e);
     }
-    connectedCallback() {
-    }
+    connectedCallback() {}
   }
   customElements.define('shop-the-look', ShopTheLook);
 }
+
+window.onload = function () {
+  lottie.loadAnimation({
+    container: document.getElementById('animation-container'), // 动画将在这个元素内部显示
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: loadingJson,
+  });
+  setTimeout(function () {
+    lottie.stop('Loading');
+  }, 5000);
+};
