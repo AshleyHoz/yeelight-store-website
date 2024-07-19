471e39bf7bfce0340d9e83a969c1f53cf24b8d69
何家伟 <hejiawei@yeelight.com>
2024-07-19 15:46:36 +0800

    添加lottie

--- assets/shop-the-look.js
+++ assets/shop-the-look.js
@@ -15,29 +15,42 @@ if (!customElements.get('shop-the-look')) {
   }
   customElements.define('shop-the-look', ShopTheLook);
 }
-function loadLottie() {
-  lottie.loadAnimation({
-    container: document.getElementById('animation-container'), // 动画将在这个元素内部显示
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: loadingJson,
-  });
-  // setTimeout(function () {
-  //   lottie.stop('Loading');
-  // }, 5000);
-}
+// function loadLottie() {
+//   lottie.loadAnimation({
+//     container: document.getElementById('animation-container'), // 动画将在这个元素内部显示
+//     renderer: 'svg',
+//     loop: true,
+//     autoplay: true,
+//     path: loadingJson,
+//   });
+//   // setTimeout(function () {
+//   //   lottie.stop('Loading');
+//   // }, 5000);
+// }
 
-document.onreadystatechange = function () {
-  console.log(1, 'lottie loaded');
+// document.onreadystatechange = function () {
+//   console.log(1, 'lottie loaded');
 
-  if (document.readyState === 'complete') {
-    // 你的代码
-    loadLottie();
-  }
-};
+//   if (document.readyState === 'complete') {
+//     // 你的代码
+//     loadLottie();
+//   }
+// };
 
-window.onload = function () {
-  console.log(2, 'lottie loaded');
-  loadLottie();
-};
+// window.onload = function () {
+//   console.log(2, 'lottie loaded');
+//   loadLottie();
+// };
+
+LottieInteractivity.create({
+  player: '#lottie-loading',
+  mode: 'scroll',
+  container: '#lottie-animation',
+  actions: [
+    {
+      visibility: [0, 1],
+      type: 'seek',
+      frames: [0, 90],
+    },
+  ],
+});
