48ac0957902705880812abeed1da3bf27b6cfb31
何家伟 <hejiawei@yeelight.com>
2024-07-22 15:26:15 +0800

    test

--- assets/shop-the-look.js
+++ assets/shop-the-look.js
@@ -2,65 +2,15 @@
  *  @class
  *  @function ShopTheLook
  */
-import lottie from 'lottie-web';
-import loadingJson from '../assets/loading.json';
-
 if (!customElements.get('shop-the-look')) {
   class ShopTheLook extends HTMLElement {
     constructor(e) {
       super();
-      console.log(e);
-
-      this.animation = lottie.loadAnimation({
-        container: document.getElementById('lottie-loading'), // 动画将在这个元素内部显示
-        renderer: 'svg',
-        loop: true,
-        autoplay: true,
-        path: loadingJson,
-      });
-
-      LottieInteractivity.create({
-        player: '#lottie-loading',
-        mode: 'scroll',
-        container: '#lottie-container',
-        actions: [
-          {
-            visibility: [0, 1],
-            type: 'seek',
-            frames: [0, 90],
-          },
-        ],
-      });
     }
     connectedCallback() {}
   }
   customElements.define('shop-the-look', ShopTheLook);
 }
-// function loadLottie() {
-lottie.loadAnimation({
-  container: document.getElementById('lottie-loading'), // 动画将在这个元素内部显示
-  renderer: 'svg',
-  loop: true,
-  autoplay: true,
-  path: loadingJson,
-});
-//   // setTimeout(function () {
-//   //   lottie.stop('Loading');
-//   // }, 5000);
-// }
-
-// document.onreadystatechange = function () {
-//   console.log(1, 'lottie loaded');
-
-//   if (document.readyState === 'complete') {
-//     loadLottie();
-//   }
-// };
-
-// window.onload = function () {
-//   console.log(2, 'lottie loaded');
-//   loadLottie();
-// };
 
 LottieInteractivity.create({
   player: '#lottie-loading',
