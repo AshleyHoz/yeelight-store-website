ed8991a7fc1af32a56826338d003d2549e8d8a59
何家伟 <hejiawei@yeelight.com>
2024-07-29 18:32:58 +0800

    shop-look调整

--- assets/shop-the-look.js
+++ assets/shop-the-look.js
@@ -6,28 +6,30 @@ if (!customElements.get('shop-the-look')) {
   class ShopTheLook extends HTMLElement {
     constructor(e) {
       super();
+      this.addEventListener('click', this.onChangeBackground);
       const currentEle = this.getElementsByClassName('shop-look-buttons-bg')[0];
       const button1 = this.getElementsByClassName('shop-look-buttons_item')[0];
-      console.log(currentEle);
+      if (!currentEl) return;
       currentEle.style.width = button1.offsetWidth + 'px';
       currentEle.style.height = button1.offsetHeight + 'px';
-      this.addEventListener('click', this.onChangeBackground);
     }
     onChangeBackground(e) {
       console.log(2, e);
-      const currentEle = this.getElementsByClassName('shop-look-buttons-bg')[0];
-      const containerEle = this.getElementsByClassName('shop-look-buttons-bg')[0];
-      const currentRect = currentEle.getBoundingClientRect();
-      const containerRect = containerEle.getBoundingClientRect();
-      console.log(e, currentEle, containerEle, currentRect, containerRect);
-      currentEle.style.width = e.offsetWidth + 'px';
-      currentEle.style.height = e.offsetHeight + 'px';
-      const offSetX = currentRect.left - containerRect.left;
-      console.log(offSetX);
-      currentEle.style.position = 'absolute';
-      currentEle.style.top = 0;
-      currentEle.style.left = offSetX + 'px';
-      currentEle.style.transform = 'translateX(' + offSetX + 'px)';
+      if (e.target.className === 'shop-look-buttons_item-content') {
+        const currentEle = this.getElementsByClassName('shop-look-buttons-bg')[0];
+        const containerEle = this.getElementsByClassName('shop-look-buttons-bg')[0];
+        const currentRect = currentEle.getBoundingClientRect();
+        const containerRect = containerEle.getBoundingClientRect();
+        console.log(e, currentEle, containerEle, currentRect, containerRect);
+        currentEle.style.width = e.offsetWidth + 'px';
+        currentEle.style.height = e.offsetHeight + 'px';
+        const offSetX = currentRect.left - containerRect.left;
+        console.log(offSetX);
+        currentEle.style.position = 'absolute';
+        currentEle.style.top = 0;
+        currentEle.style.left = offSetX + 'px';
+        currentEle.style.transform = 'translateX(' + offSetX + 'px)';
+      }
     }
     connectedCallback() {}
   }
