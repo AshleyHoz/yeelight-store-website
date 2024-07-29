395f9088426b178ff4cbd09c28fd2978dfddd70c
何家伟 <hejiawei@yeelight.com>
2024-07-29 15:37:09 +0800

    shop-look调整

--- assets/shop-the-look.js
+++ assets/shop-the-look.js
@@ -6,8 +6,33 @@ if (!customElements.get('shop-the-look')) {
   class ShopTheLook extends HTMLElement {
     constructor(e) {
       super();
+      this.addEventListener('change', this.changeBackground);
+    }
+    changeBackground(e) {
+      console.log(e);
+      const buttons = document.querySelectorAll('.shop-look-buttons_item');
+      buttons.forEach(button => {
+        button.classList.remove('active');
+      });
+    }
+    onChangeBackground(e) {
+      console.log(2, e);
     }
     connectedCallback() {}
   }
   customElements.define('shop-the-look', ShopTheLook);
 }
+
+function onChangeBackground(e) {
+  console.log(1, e);
+  const buttons = document.querySelectorAll('.shop-look-buttons_item');
+  buttons.forEach(button => {
+    button.classList.remove('active');
+  });
+}
+
+const btn1 = document.getElementById('button-1');
+const btn2 = document.getElementById('button-2');
+const btn3 = document.getElementById('button-3');
+const btn4 = document.getElementById('button-4');
+const btn5 = document.getElementById('button-5');
