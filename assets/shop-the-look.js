1bc26f86e5acdff8b4cf53e325cb8207f26e2a75
何家伟 <hejiawei@yeelight.com>
2024-09-26 14:03:10 +0800

    ui调整

--- assets/shop-the-look.js
+++ assets/shop-the-look.js
@@ -7,7 +7,6 @@ if (!customElements.get('shop-the-look')) {
     constructor(e) {
       super();
       this.addEventListener('click', this.onChangeBackground);
-      this.addEventListener('touchmove', this.onChangeBackground);
       const currentEle = this.getElementsByClassName('shop-look-buttons-bg')[0];
       const button1 = this.getElementsByClassName('shop-look-buttons_item')[0];
       if (!currentEle) return;
@@ -16,31 +15,32 @@ if (!customElements.get('shop-the-look')) {
       button1.classList.add('active');
     }
     onChangeBackground(e) {
-      console.log(e);
+      // if (
+      //   e.target.className === 'shop-look-buttons_item-content' ||
+      //   e.target.parentNode.className === 'shop-look-buttons_item-content'
+      // ) {
+      const coverEle = this.getElementsByClassName('shop-look-buttons-bg')[0];
+      const currentEle = this.getElementsByClassName('shop-look-buttons_item is-selected');
+      console.log(currentEle);
 
-      if (
-        e.target.className === 'shop-look-buttons_item-content' ||
-        e.target.parentNode.className === 'shop-look-buttons_item-content'
-      ) {
-        const coverEle = this.getElementsByClassName('shop-look-buttons-bg')[0];
-        const currentEle = e.target.className === 'shop-look-buttons_item-content' ? e.target : e.target.parentNode;
-        const containerEle = this.getElementsByClassName('shop-look-buttons')[0];
-        const buttonsEle = this.getElementsByClassName('shop-look-buttons_item');
-        const buttonItemEle = currentEle.parentNode;
-        const currentRect = currentEle.getBoundingClientRect();
-        const containerRect = containerEle.getBoundingClientRect();
-        const offSetX = currentRect.left - containerRect.left;
-        coverEle.style.width = currentEle.offsetWidth + 'px';
-        coverEle.style.height = currentEle.offsetHeight + 'px';
-        coverEle.style.position = 'absolute';
-        coverEle.style.top = 0;
-        coverEle.style.left = 0;
-        coverEle.style.transform = 'translateX(' + offSetX + 'px)';
-        for (let i = 0; i < buttonsEle.length; i++) {
-          buttonsEle[i].classList.remove('active');
-        }
-        buttonItemEle.classList.add('active');
+      // e.target.className === 'shop-look-buttons_item-content' ? e.target : e.target.parentNode;
+      const containerEle = this.getElementsByClassName('shop-look-buttons')[0];
+      const buttonsEle = this.getElementsByClassName('shop-look-buttons_item');
+      const buttonItemEle = currentEle.parentNode;
+      const currentRect = currentEle.getBoundingClientRect();
+      const containerRect = containerEle.getBoundingClientRect();
+      const offSetX = currentRect.left - containerRect.left;
+      coverEle.style.width = currentEle.offsetWidth + 'px';
+      coverEle.style.height = currentEle.offsetHeight + 'px';
+      coverEle.style.position = 'absolute';
+      coverEle.style.top = 0;
+      coverEle.style.left = 0;
+      coverEle.style.transform = 'translateX(' + offSetX + 'px)';
+      for (let i = 0; i < buttonsEle.length; i++) {
+        buttonsEle[i].classList.remove('active');
       }
+      buttonItemEle.classList.add('active');
+      // }
     }
     connectedCallback() {}
   }
