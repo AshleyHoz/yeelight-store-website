628f0b1e0dacbad18ed0de50b4c51f14ed064bcf
何家伟 <hejiawei@yeelight.com>
2024-10-28 11:22:10 +0800

    shop the look slider

--- assets/shop-the-look-slider.js
+++ assets/shop-the-look-slider.js
@@ -7,18 +7,18 @@ if (!customElements.get('shop-the-look-slider')) {
     constructor(e) {
       super();
       this.addEventListener('click', this.onChangeBackground);
-      // const coverEle = this.getElementsByClassName('shop-look-buttons-bg')[0];
-      const button1 = this.getElementsByClassName('shop-look-buttons_item')[0];
+      // const coverEle = this.getElementsByClassName('shop-look-slider-buttons-bg')[0];
+      const button1 = this.getElementsByClassName('shop-look-slider-buttons_item')[0];
       // if (!coverEle) return;
       // coverEle.style.width = button1.offsetWidth + 'px';
       // coverEle.style.height = button1.offsetHeight * 0.5 + 'px';
       button1.classList.add('active');
     }
     onChangeBackground(e) {
-      const coverEle = this.getElementsByClassName('shop-look-buttons-bg')[0];
-      const currentEle = this.getElementsByClassName('shop-look-buttons_item is-selected')[0];
-      const containerEle = this.getElementsByClassName('shop-look-buttons')[0];
-      const buttonsEle = this.getElementsByClassName('shop-look-buttons_item');
+      const coverEle = this.getElementsByClassName('shop-look-slider-buttons-bg')[0];
+      const currentEle = this.getElementsByClassName('shop-look-slider-buttons_item is-selected')[0];
+      const containerEle = this.getElementsByClassName('shop-look-slider-buttons')[0];
+      const buttonsEle = this.getElementsByClassName('shop-look-slider-buttons_item');
       const currentRect = currentEle.getBoundingClientRect();
       const containerRect = containerEle.getBoundingClientRect();
       const offSetX = currentRect.left - containerRect.left;
